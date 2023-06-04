2b89ba7e4f6139749e0499f685805cef40d93b2a
thaiduong16012001 <thaiduong16012001@gmail.com>
2023-06-05 01:40:49 +0700

    #6 update 5/6/2023

--- src/pages/userLog/Userlog.tsx
+++ src/pages/userLog/Userlog.tsx
@@ -10,8 +10,72 @@ function Userlog() {
       <Sidebar></Sidebar>
       <Logging></Logging>
       <div className="table">
-        <div className="table_top">ahihi</div>
-        <div className="table_body">ahihihihih</div>
+        <div className="table_top">Chọn thời gian</div>
+        <div className="table_body">
+        <table>
+          <tr>
+            <th className="name__column">Tên đăng nhập</th>
+            <th className="name__column">Thời gian tác động</th>
+            <th className="name__column">IP thực hiện</th>
+            <th className="name__column">Thao tác thực hiện</th>
+          </tr>
+          <tr>
+            <td>Kế toán</td>
+            <td>6</td>
+            <td>Thực hiện nhiệm vụ về thống kê số liệu và tổng hợp số liệu</td>
+            <td>Cập nhật</td>
+          </tr>
+          <tr>
+            <td>Bác sĩ</td>
+            <td>6</td>
+            <td>Thực hiện nhiệm vụ về thống kê số liệu và tổng hợp số liệu</td>
+            <td>Cập nhật</td>
+          </tr>
+          <tr>
+            <td>Lễ tân</td>
+            <td>6</td>
+            <td>Thực hiện nhiệm vụ về thống kê số liệu và tổng hợp số liệu</td>
+            <td>Cập nhật</td>
+          </tr>
+          <tr>
+            <td>Quản lý</td>
+            <td>6</td>
+            <td>Thực hiện nhiệm vụ về thống kê số liệu và tổng hợp số liệu</td>
+            <td>Cập nhật</td>
+          </tr>
+          <tr>
+            <td>Admin</td>
+            <td>6</td>
+            <td>Thực hiện nhiệm vụ về thống kê số liệu và tổng hợp số liệu</td>
+            <td>Cập nhật</td>
+          </tr>
+          <tr>
+            <td>Super Amin</td>
+            <td>6</td>
+            <td>Thực hiện nhiệm vụ về thống kê số liệu và tổng hợp số liệu</td>
+            <td>Cập nhật</td>
+          </tr>
+          <tr>
+            <td>Super Amin</td>
+            <td>6</td>
+            <td>Thực hiện nhiệm vụ về thống kê số liệu và tổng hợp số liệu</td>
+            <td>Cập nhật</td>
+          </tr>
+          <tr>
+            <td>Super Amin</td>
+            <td>6</td>
+            <td>Thực hiện nhiệm vụ về thống kê số liệu và tổng hợp số liệu</td>
+            <td>Cập nhật</td>
+          </tr>
+          <tr>
+            <td>Super Amin</td>
+            <td>6</td>
+            <td>Thực hiện nhiệm vụ về thống kê số liệu và tổng hợp số liệu</td>
+            <td>Cập nhật</td>
+          </tr>
+          
+        </table>
+        </div>
         <div className="paging">
           <a href="/">&laquo;</a>
           <a href="/">1</a>
